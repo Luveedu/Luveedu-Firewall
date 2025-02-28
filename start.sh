80a9f895cc71bfbc42f026793cfc584c7271611c
Ariyan Debnath <88545251+ariyandnath@users.noreply.github.com>
2025-02-28 20:38:26 +0530

    Minor Bug Resolved!

--- start.sh
+++ start.sh
@@ -67,7 +67,9 @@ install_luvd_firewall() {
         echo "luvd-firewall updated at $target"
         # Just reset since it already existed
         echo "Running luvd-firewall --reset..."
+        sleep 5
         "$target" --reset
+        sleep 2
         figlet "Updated"
         echo "Luveedu Firewall updated successfully!"
         exit 0
